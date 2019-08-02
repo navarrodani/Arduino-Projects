49129524ba2e3c481db1b575f101482bae34df1f
Dani Navarro <46326039+navarrodani@users.noreply.github.com>
2019-08-02 11:35:46 +0200

    Add files via upload

--- Arduino_Timelapse/Arduino_Timelapse.ino
+++ Arduino_Timelapse/Arduino_Timelapse.ino
@@ -7,14 +7,16 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 Servo name_servo;
 int servo_position = 0;
 int LEDPin = 7;
+int LEDPin_b = 8;
 int fotos;
-int time_interval = 400; // ASSUMING THE GOPRO DOES A PHOTO EVERY 2 SECONDS, FOR EVERY DEGREE TURNED IT MAKES 4 PHOTOS. CHANGE THIS VALUE DEPENDING ON THE RELATION SPEED/PHOTOS YOU WANT.
-
+int time_interval = 4000; // ASSUMING THE GOPRO DOES A PHOTO EVERY 2 SECONDS, FOR EVERY DEGREE TURNED IT MAKES 4 PHOTOS. CHANGE THIS VALUE DEPENDING ON THE RELATION SPEED/PHOTOS YOU WANT.
+int i=0;
 // Setup
 
 void setup() {
   // Define LED
   pinMode(LEDPin, OUTPUT);
+  pinMode(LEDPin_b, OUTPUT);
   // Inicialice Servo
   name_servo.attach(4);
   // Indicate the library we are using a 16x2
@@ -32,11 +34,11 @@ void setup() {
   // Wait for a couple seconds
   delay(2000);
   digitalWrite(LEDPin, LOW);
-  
 }
 
 void loop() {
-  // Servo turning from 0 to 180 degrees
+if (i<1) {
+    // Servo turning from 0 to 180 degrees
  for (servo_position=0; servo_position <=180; servo_position +=1){
     // Update the number of photos made
     fotos = (servo_position*2);
@@ -57,7 +59,7 @@ void loop() {
   // Servo turning from 180 to 0 degrees
   for (servo_position=180; servo_position >=0; servo_position -=1){
     // Update the number of photos made
-    fotos = ((servo_position*((time_interval/100)/2)) + ((180-servo_position)*(time_interval/100)));
+    fotos = ((servo_position*((time_interval/1000)/2)) + ((180-servo_position)*(time_interval/1000)));
     // Update the LCD
     lcd.clear();
     name_servo.write(servo_position);
@@ -70,5 +72,20 @@ void loop() {
     lcd.print("PHOTOS:  ");
     lcd.print(fotos);
     delay(time_interval);
-      }
+    }  
+  i+=1;
+  }
+  else {
+     lcd.clear();
+     lcd.print("TIME LAPSE DONE!");
+     lcd.setCursor ( 0, 1 );
+     lcd.print("N");
+     lcd.print((char)223);
+     lcd.print("PHOTOS:  ");
+     lcd.print(fotos);
+     digitalWrite(LEDPin_b, HIGH);
+     delay(500);
+     digitalWrite(LEDPin_b, LOW);
+     delay(500);
+        }
  }
